c02c7158e2d142432a7376a97ddf192c794ae637
sebastianseidelmann <sebastian.seidelmann@wfp2.com>
2016-04-05 14:53:09 +0200

    [TASK] ADDED the basic translation

---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Extension Manager configuration file for ext "static_info_tables_de"
+ */
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Static Info Tables (de)',
+	'description' => 'German (de) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
+	'category' => 'misc',
+	'version' => '6.3.9',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'author' => 'Sebastian Seidelmann',
+	'author_email' => 'sebastian.seidelmann@googlemail.com',
+	'author_company' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-7.6.99',
+			'static_info_tables' => '6.3.9-'
+		),
+		'conflicts' => array(),
+		'suggests' => array()
+	)
+);
