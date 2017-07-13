82b929599e8b2d83472d0075fee699e5ac38fbd7
Dennis Rheinfelder <dennis.rheinfelder@wfp2.com>
2017-07-13 14:40:05 +0200

    [TASK] Pepare for TYPO3 8

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Static Info Tables (de)',
 	'description' => 'German (de) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
 	'category' => 'misc',
-	'version' => '6.3.9',
+	'version' => '6.4.0',
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.6.99',
+			'typo3' => '6.2.0-8.7.99',
 			'static_info_tables' => '6.3.9-'
 		),
 		'conflicts' => array(),
